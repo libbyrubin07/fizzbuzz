1903e80d2c230a0e584fabcafd4b09d768865c51
libbyrubin07 <libby.rubin@icloud.com>
2021-04-09 18:51:45 +0100

    removed commented code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,7 @@
 
 for number in range (1,101) :
     message = ""
-    
-#     if (number%3==0) and (number%5==0):
-#         print("fizzbuzz")
+
     if (number%3)==0:
         message += "fizz"
     if (number%5)==0:
