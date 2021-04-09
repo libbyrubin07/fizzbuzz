678049e9a265773d4f4b90a3ab8f24f176d1a305
libbyrubin07 <libby.rubin@icloud.com>
2021-04-09 18:43:34 +0100

    Added fizzbuzz file

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+
+for number in range (1,101) :
+    message = ""
+    
+#     if (number%3==0) and (number%5==0):
+#         print("fizzbuzz")
+    if (number%3)==0:
+        message += "fizz"
+    if (number%5)==0:
+        message += "buzz"
+    if (number%7)==0:
+        message += "hazz"
+    print(number, message)
